e6b744bb37d4e0f11a6b4f90561b53707c096a3e
LuciaPuppo897 <alerome897@gmail.com>
2024-01-25 06:42:57 -0800

    add task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+if (process.argv.length < 3) {
+  console.error('Please provide the JSONPlaceholder API URL as an argument');
+  process.exit(1);
+}
+
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const tasksData = JSON.parse(body);
+
+    const completedTasks = tasksData.filter(task => task.completed);
+
+    const completedTasksByUser = completedTasks.reduce((acc, task) => {
+      const userId = task.userId;
+      acc[userId] = (acc[userId] || 0) + 1;
+      return acc;
+    }, {});
+
+    console.log(completedTasksByUser);
+  }
+});
