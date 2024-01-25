04d9b8d80314f619b6dd9059157a690e3560f6c1
LuciaPuppo897 <alerome897@gmail.com>
2024-01-25 05:59:44 -0800

    add task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+    console.error('Please provide both the file path and the string to write as arguments');
+    process.exit(1);
+}
+
+
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+    if (err) {
+        
+        console.error(err);
+    } else {
+        console.log(`Content has been successfully written to ${filePath}`);
+    }
+});
