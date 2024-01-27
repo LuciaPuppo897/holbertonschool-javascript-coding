d56908d53e90b4d2a95c0b31a6f33ec40eb616d0
LuciaPuppo897 <alerome897@gmail.com>
2024-01-27 15:17:49 -0800

    fixed task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,24 +2,15 @@
 const request = require('request');
 const fs = require('fs');
 
-if (process.argv.length < 4) {
-  console.error('Please provide both the URL and the file path as arguments');
-  process.exit(1);
-}
+const [, , URL, filePath] = process.argv;
 
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request.get(url, (error, response, body) => {
+request.get(URL, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
-      if (writeError) {
-        console.error(writeError);
-      } else {
-        console.log(`Body response has been stored in ${filePath}`);
-      }
-    });
   }
+  fs.writeFile(filePath, body, 'utf-8', (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
 });
