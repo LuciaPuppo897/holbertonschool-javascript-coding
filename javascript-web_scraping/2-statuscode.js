df98ccb01c7146a79c99831e5b2e985286966ea1
LuciaPuppo897 <alerome897@gmail.com>
2024-01-26 20:10:43 -0800

    fixed task 1 n 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 
-if (process.argv.length < 3) {
-  console.error('Please provide the URL as an argument');
-  process.exit(1);
-}
-
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
